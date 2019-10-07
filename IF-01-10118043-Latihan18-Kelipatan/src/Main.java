99681e7ea0e6e8a9fbd7b82e708de0937bef3657
Dian Rosa Pratama <55683288+DianRP43@users.noreply.github.com>
2019-10-07 12:17:55 +0700

    Add files via upload

---
+++ IF-01-10118043-Latihan18-Kelipatan/src/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a;
+        a = 3.5;
+        do{
+            System.out.println(a);
+            a = a + 3.5;
+        }while(a <= 35);
+    }
+    
+}
